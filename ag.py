e5c2e9d68f52d0420a9c6e11fabcf7c6a0154620
Armando <armandoalanalmeida@gmail.com>
2016-08-31 15:05:17 -0300

    Estrutura do código

--- ag.py
+++ ag.py
@@ -1,1 +1,13 @@
-import rand 
+from random import randint, uniform
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+
+numcolunas = 10 #Numero de colunas da matriz 
+numlinhas = 10 #Num de linhas da Matriz
+
+
+def criarpop(): 
+
+def fitness():
+
