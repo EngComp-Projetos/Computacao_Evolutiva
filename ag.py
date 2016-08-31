b5e4c2bb235c583b93e709bfd7d6b3d8631db340
Edilson Santiago <edilson.engcomp@gmail.com>
2016-08-31 16:00:28 -0300

    individuos

--- ag.py
+++ ag.py
@@ -7,16 +7,27 @@ import numpy as np
 
 numcolunas = 3  # Número de colunas da matriz
 numlinhas = 3  # Número de linhas da matriz
-numeroindividuos = 3  # Quantidade de trilhas na placa
+numerotrilhas = 3  # Quantidade de trilhas na placa
+populacao = []
+tamanhopopulacao = 50
 
 
 def criarpop(): 
-    populacao = np.zeros((numeroindividuos, numlinhas, numcolunas))
-    # print(populacao)
-    print(populacao[0][0][0])
+    for i in range(0, tamanhopopulacao):
+        populacao.append(geraindividuos())
+    print(populacao[0])
+
+
+def geraindividuos():
+    individuo = np.zeros((numerotrilhas, numlinhas, numcolunas))
+    # print(individuo)
+    # print(individuo[0][0][0])
+
+    return individuo
 
 
 def fitness():
     pass
+
 
 criarpop()
