d7ca813bdc28e8ffc0a286cded6f53e8d37b73e1
Edilson Santiago <edilson.engcomp@gmail.com>
2016-08-31 15:35:18 -0300

    Matriz

--- ag.py
+++ ag.py
@@ -3,11 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import csv
 
-numcolunas = 10 #Numero de colunas da matriz 
-numlinhas = 10 #Num de linhas da Matriz
-
+numcolunas = 3 #Numero de colunas da matriz
+numlinhas = 3 #Num de linhas da Matriz
+numeroindividuos = 3
 
 def criarpop(): 
+    populacao = np.zeros((numeroindividuos, numlinhas, numcolunas))
+    # print(populacao)
+    print(populacao[0][0][0])
 
 def fitness():
+    pass
 
+criarpop()
