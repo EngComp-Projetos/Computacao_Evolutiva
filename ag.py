569624800aca5c44ee6394eb9c28ad7bba115b31
Edilson Santiago <edilson.engcomp@gmail.com>
2016-08-31 15:38:38 -0300

    ajustes

--- ag.py
+++ ag.py
@@ -1,16 +1,18 @@
-from random import randint, uniform
 import numpy as np
-import matplotlib.pyplot as plt
-import csv
+# from random import randint, uniform
+# import matplotlib.pyplot as plt
+# import csv
 
-numcolunas = 3 #Numero de colunas da matriz
-numlinhas = 3 #Num de linhas da Matriz
+numcolunas = 3  # Numero de colunas da matriz
+numlinhas = 3  # Numero de linhas da Matriz
 numeroindividuos = 3
+
 
 def criarpop(): 
     populacao = np.zeros((numeroindividuos, numlinhas, numcolunas))
     # print(populacao)
     print(populacao[0][0][0])
+
 
 def fitness():
     pass
